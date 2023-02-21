ea58bd6f9dd86feb319f8f87b136714aedc1473c
kyawlinnthant <kyawlinnthantkyawlinnthant@gmail.com>
2023-02-21 10:18:20 +0630

    monolithic catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
     }
 }
 
-@Suppress("DSL_SCOPE_VIOLATION")
+@Suppress("DSL_SCOPE_VIOLATION")// Remove when fixed https://youtrack.jetbrains.com/issue/KTIJ-19369
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
